97d8d95465c3e35b3212776a0df6be543642d0d2
Carlos Barajas <soldotsol@gmail.com>
2024-03-21 18:07:32 -0700

    Show job achievements to job history

--- solthoth/src/app/job/job.component.ts
+++ solthoth/src/app/job/job.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { Job } from '../job';
@@ -7,7 +8,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-job',
   standalone: true,
-  imports: [MatCardModule, RouterModule, MatListModule],
+  imports: [MatCardModule, RouterModule, MatListModule, CommonModule],
   templateUrl: './job.component.html',
   styleUrl: './job.component.css'
 })
