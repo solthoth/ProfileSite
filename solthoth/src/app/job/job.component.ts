5899bea90c4270ecc172f51cffe4d6b3c16a6c91
Brett Sanders <blogbrett@gmail.com>
2025-10-05 19:06:52 -0700

    Enhance styling and layout for app components; introduce global design tokens, improve responsiveness, and refine structure for better user experience.

--- solthoth/src/app/job/job.component.ts
+++ solthoth/src/app/job/job.component.ts
@@ -1,14 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
 import { Job } from '../job';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-job',
   standalone: true,
-  imports: [MatCardModule, RouterModule, MatListModule, CommonModule],
+  imports: [MatCardModule, RouterModule, CommonModule],
   templateUrl: './job.component.html',
   styleUrl: './job.component.css'
 })
